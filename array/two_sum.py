01f55b994f1e874f412327dddf20d326d9aa6c9b
Harbin ks <harbinks88@gmail.com>
2025-12-29 00:33:45 +0530

    added print in vs code

--- array/two_sum.py
+++ array/two_sum.py
@@ -7,3 +7,9 @@ class Solution:
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    sol = Solution()
+    print(sol.twoSum(nums, target))
